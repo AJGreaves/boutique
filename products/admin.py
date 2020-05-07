5b8ca94a358174fea1898ec6f502e307db9dd857
A Greaves <gilhespy.anna@gmail.com>
2020-05-07 07:49:37 +0000

    Set tables for product and category data in admin panel.

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,30 @@ from django.contrib import admin
 from .models import Product, Category
 
 
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    """
+    Adds extra fields in admin panel so you can see product
+    data all in one table.
+    """
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    # Adds fields to view in admin panel.
+    list_display = (
+        'friendly_name',
+        'name'
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
